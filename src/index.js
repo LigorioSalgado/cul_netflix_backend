b50074eef2dab43a71cec7ed246fab54c79c8d79
Edwin Salgado <edwin@devf.mx>
2018-12-10 20:45:27 -0700

    changes for cors

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const options = {
     playground:'/playground',
     cors:{
         credentials:true,
-        origin:["http://localhost:3000","https://mynetflix.netlify.com"]
+        origin:["http://localhost:3000","https://*.netlify.com"]
     }
 
 
